5572ac2e7cdec6b49dd00ae258f1f84a843fd57a
belmont <belmont.claire1@gmail.com>
2023-07-05 16:19:27 +0200

    Ajout des fichiers sur le produit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-
-using namespace std;
+#include "produit.hpp"
+//#include "market.hpp"
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    struct Produit p1;
+    struct Produit p2;
+    struct Produit p3;
+
+//    Market::constructeurMarket();
+//
+//    Market::getInstance()->ajouterProduit(&p1);
+//    Market::getInstance()->ajouterProduit(&p2);
+//    Market::getInstance()->ajouterProduit(&p3);
+//
+//    delete Market::getInstance();
+
     return 0;
 }
--- produit.hpp
+++ produit.hpp
@@ -11,6 +11,11 @@ struct Produit
     int idEntreprise;
     int quantite;
     int qualite;
+    Produit(){};
+    Produit(int _idProduit, float _prix, int _idEntreprise, int _quantite, int _qualite):
+        idProduit(_idProduit),prix(_prix),idEntreprise(_idEntreprise), quantite(_quantite),qualite(_qualite)
+    {
+    }
 };
 
 #endif
