550fe5c93b788ba0a03f581eac6b202b0c15c993
belmont <belmont.claire1@gmail.com>
2023-07-05 15:09:57 +0200

    Ajout des fichiers de la classe produit

---
+++ produit.hpp
@@ -0,0 +1,16 @@
+// CLAIRE Belmont
+#ifndef PRODUIT_H
+#define PRODUIT_H
+#include <iostream>
+
+
+struct Produit
+{
+    int idProduit;
+    float prix;
+    int idEntreprise;
+    int quantite;
+    int qualite;
+};
+
+#endif
