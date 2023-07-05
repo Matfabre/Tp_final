ef7859bfe527f9bba3fe627a2a3a286caedead36
pierreantoinesoyer <pierreantoine.soyer@reseau.eseo.fr>
2023-07-05 15:35:34 +0200

    ajout de la structure Client Automate CompteRendu ListeProduit

---
+++ Client.hpp
@@ -0,0 +1,18 @@
+class Client
+{
+private:
+    float salaire;
+    float argent;
+
+public:
+    Client(float _salaire, float _argent);
+    void shop();
+    ~Client();
+
+private:
+    void gagneSalaire();
+
+};
+
+
+
