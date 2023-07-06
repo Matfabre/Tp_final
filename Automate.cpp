9dd8fc072015c71c5d496c26f5ebe274b484d2a6
belmont <belmont.claire1@gmail.com>
2023-07-06 12:13:50 +0200

    Initialisation des clients et des entreprises

--- Automate.cpp
+++ Automate.cpp
@@ -1,7 +1,32 @@
 #include "Automate.hpp"
+#include <Time.h>
 
 Automate::Automate()
 {
+    Entreprise SuperMeuble("SuperMeuble",1);
+    Entreprise MeubleHyper("MeubleHyper",2);
+    Entreprise DecoMaison("DecoMaison",3);
+    Entreprise CuisineOutil("CuisineOutil",4);
+    Entreprise MonJardin("MonJardin",5);
+
+    getInstance()->addEntreprise(SuperMeuble);
+    getInstance()->addEntreprise(MeubleHyper);
+    getInstance()->addEntreprise(DecoMaison);
+    getInstance()->addEntreprise(CuisineOutil);
+    getInstance()->addEntreprise(MonJardin);
+
+    srand((unsigned) time(NULL));
+
+    // Creation de 20 clients
+    for(int i = 0; i<20; i++)
+    {
+        // salaire est un nombre au hasard entre 0 et 3000
+        float salaire = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/3000));
+        // argent est un nombre au hasard entre 0 et 3000
+        float argent = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/3000));
+        Client client(salaire, argent);
+        getInstance()->addClient(client);
+    }
 }
 
 void Automate::addEntreprise(const Entreprise& entreprise)
