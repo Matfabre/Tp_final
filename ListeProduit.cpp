1ea0f827ea12144d1746e542af8650dbafd7217b
pierreantoinesoyer <68423459+pierreantoinesoyer@users.noreply.github.com>
2023-07-07 16:17:52 +0200

    comments ListeProduit.cpp

--- ListeProduit.cpp
+++ ListeProduit.cpp
@@ -1,6 +1,8 @@
+//author : Pierre-Antoine
+//cette classe sert à accéder aux noms des produits à partir de leur id
 #include "ListeProduit.hpp"
 
-std::string ListeProduit::operator[](int id) const
+std::string ListeProduit::operator[](int id) const  //surcharge de l'opérateur [] pour rendre l'accessibilité en dictionnaire
 {
     return produits.at(id);
 }
