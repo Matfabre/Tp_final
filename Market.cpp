3e243685191753d405eebb9c169794f0b3520d8f
pierreantoinesoyer <68423459+pierreantoinesoyer@users.noreply.github.com>
2023-07-07 16:26:39 +0200

    comments Market.cpp

--- Market.cpp
+++ Market.cpp
@@ -15,29 +15,30 @@ Market *Market::getInstance()
     return instance;
 }
 
-// methode pour ajouter un produit
+//author Pierre-Antoine
+// methode pour ajouter un produit fini
 void Market::ajouterProduitFini(Produit* produit)
 {
     for(int i = 0; i < produitsFinis.size(); i++)
     {
-        if(produit->idProduit == produitsFinis[i]->idProduit && produit->idEntreprise == produitsFinis[i]->idEntreprise)
-        {
+        if(produit->idProduit == produitsFinis[i]->idProduit && produit->idEntreprise == produitsFinis[i]->idEntreprise)  //si le produit est déjà connu
+        {                                                                                                    //il suffit d'augmenter la quantité
             produitsFinis[i]->quantite += produit->quantite;
-            delete produit;
+            delete produit;  //on supprime l'objet produit en mémoire, il existe déjà un produit dans le marché qui sera supprimé au destructeur
             return;
         }
     }
-    produitsFinis.push_back(produit);
+    produitsFinis.push_back(produit); //sinon on ajoute le produit au vecteur
 }
-
+// methode pour ajouter une matiere première au marché
 void Market::ajouterMatierePremiere(Produit* produit)
 {
     for(int i = 0; i < matieresPremieres.size(); i++)
-    {
+    { //si le produit est déjà connu il suffit d'augmenter la quantité
         if((produit->idProduit == matieresPremieres[i]->idProduit) && (produit->idEntreprise == matieresPremieres[i]->idEntreprise))
         {
             matieresPremieres[i]->quantite += produit->quantite;
-            delete produit;
+            delete produit; //on supprime l'objet produit en mémoire, il existe déjà un produit dans le marché qui sera supprimé au destructeur
             return;
         }
     }
@@ -48,14 +49,14 @@ void Market::ajouterMatierePremiere(Produit* produit)
 // Destucteur de Market
 Market::~Market()
 {
-    for (Produit* produit : produitsFinis) {
+    for (Produit* produit : produitsFinis) {  //on supprime toutes les structures produit
         delete produit;
     }
     for (Produit* produit : matieresPremieres) {
         delete produit;
     }
 
-    delete(instance);
+    delete(instance);  
 }
 
 // Constructeur de Market
@@ -73,7 +74,7 @@ std::vector<Produit*> Market::getMatieresPremieres()
     return matieresPremieres;
 }
 
-void Market::transaction(Produit* produit, int quantiteAchete)
+void Market::transaction(Produit* produit, int quantiteAchete)  //envoie l'argent de la transaction au marchand
 {
     (produit->quantite) -= quantiteAchete;
     std::vector<EntrepriseProduitFinis*> marchandsProduitsFinis = Automate::getInstance()->getEntreprisesProduitFinis();
@@ -84,7 +85,7 @@ void Market::transaction(Produit* produit, int quantiteAchete)
 
         if(marchand->getIdEntreprise() == produit->idEntreprise)
         {
-            marchand->recois(produit->prix * quantiteAchete);
+            marchand->recois(produit->prix * quantiteAchete);  //on envoie la somme a l'entreprise ayant le bon id
             break;
         }
     }
@@ -105,4 +106,4 @@ int Market::nombreInstances = 0;
 
 
 
-Market *Market::instance = NULL;
+Market *Market::instance = NULL;  //initialisation de l'instance à null
